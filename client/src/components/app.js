05d8c13e94f4b2f0e51ed7039ac511072db8d895
Brian Bier <briansbier@gmail.com>
2017-01-19 00:02:15 -0500

    Set up layout app

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,1 +1,18 @@
-app.js
+import React, {Component} from 'react';
+
+class App extends Component{
+	render(){
+		return(
+			<div>
+				<p>Just Search</p>
+				<div class="container">
+					{React.cloneElement(this.props.children, this.props)}
+				</div>
+				<p>Just Search Footer</p>
+			</div>
+		)
+	}
+}
+
+export default App;
+
